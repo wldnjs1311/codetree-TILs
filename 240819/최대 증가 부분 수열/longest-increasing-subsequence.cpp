b205add66b95609d1178402d3c68b8015dd0686d
Noremio <84217827+wldnjs1311@users.noreply.github.com>
2024-08-19 21:21:40 +0900

    Title: 최대 증가 부분 수열, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 240819/최대 증가 부분 수열/longest-increasing-subsequence.cpp
+++ 240819/최대 증가 부분 수열/longest-increasing-subsequence.cpp
@@ -34,7 +34,7 @@ int main() {
   int maxLength = 1;
   for (int i = 1; i < n; i++) {
     for (int j = i - 1; j >= 0; j--) {
-      if (nums[i] > nums[j] && dp[i] < nums[j] + 1) {
+      if (nums[i] > nums[j] && dp[i] < dp[j] + 1) {
         dp[i] = dp[j] + 1;
         maxLength = max(maxLength, dp[i]);
       }
