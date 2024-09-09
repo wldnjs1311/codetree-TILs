a978d444bbc92d071c2afbdc2a0316fcc143b77f
Noremio <84217827+wldnjs1311@users.noreply.github.com>
2024-09-09 17:27:38 +0900

    Title: 가장 가까운 점, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240909/가장 가까운 점/nearest-point.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <queue>
+#include <list>
+#include <deque>
+#include <cmath>
+
+using namespace std;
+
+struct cmp
+{
+	bool operator() (pair<int,int>& a, pair<int,int>& b)
+	{
+		return (abs(a.first) + abs(a.second)) > (abs(b.first) + abs(b.second));
+	}
+};
+
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+	priority_queue<pair<int,int>, vector<pair<int,int>>, cmp> pq;
+
+	int n, m;
+	cin >> n >> m;
+
+	for (int i = 0; i < n; i++)
+	{
+		int a, b;
+		cin >> a >> b;
+		pq.push({ a,b });
+	}
+
+	for (int i = 0; i < m; i++)
+	{
+		//원점에서 가장 가까운 점을 골라서 x, y에 2씩 증가
+		pair<int, int> tmp = pq.top();
+		tmp.first += 2;
+		tmp.second += 2;
+
+		pq.pop();
+		pq.push(tmp);
+	}
+
+	cout << pq.top().first << ' ' << pq.top().second;
+}
