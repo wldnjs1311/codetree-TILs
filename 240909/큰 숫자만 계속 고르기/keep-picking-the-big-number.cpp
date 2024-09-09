eb6e42c574a071f11db8dd835d3a2d920a8f224e
Noremio <84217827+wldnjs1311@users.noreply.github.com>
2024-09-09 16:46:56 +0900

    Title: 큰 숫자만 계속 고르기, Time: 25ms, Memory: 1MB, Status: Passed - CodeTree

---
+++ 240909/큰 숫자만 계속 고르기/keep-picking-the-big-number.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+	int N, M;
+	cin >> N >> M;
+
+	priority_queue<long long> pq;
+
+	for (int i = 0; i < N; i++)
+	{
+		int a;
+		cin >> a;
+		pq.push(a);
+	}
+
+	for (int i = 0; i < M; i++)
+	{
+		int tmp = pq.top();
+		tmp--;
+		pq.pop();
+		pq.push(tmp);
+	}
+
+	cout << pq.top();
+}
